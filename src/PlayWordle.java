514acfa9e1751577351154e7eb2b5e0ab0e2c34a
cardinn454_lpsk12 <Cardinn454@lpsk12.org>
2023-09-01 09:32:45 -0600

    I created the game Wordle.

--- src/PlayWordle.java
+++ src/PlayWordle.java
@@ -1,3 +1,82 @@
-public class PlayWordle {
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class PlayWordle {
+    public static void main(String[] args) {
+        // Gets a list of words and makes it the secret word, the reason that Math.random() is multiplied by 1001 is because otherwise it won't include the 1000th word in the list.
+
+        String[] wordList = getWords();
+        Wordle wordleGame = new Wordle(wordList[(int)(Math.floor(Math.random() * 1001))]);
+
+        System.out.println("Your 5 letter Wordle word is waiting!");
+
+        // The code that runs all the guesses
+        Scanner guesses = new Scanner(System.in);
+        for (int i = 1; i < 7; i++){
+
+            // prints out all the available letters to the user and what guess they are on
+
+            System.out.println("Possible Letters: " + wordleGame.getLetters());
+            System.out.print("What is guess " + i + " --> ");
+
+            // extracts the guess from the user and generates a hint based on the guess and secret word
+
+            String guess = guesses.nextLine();
+
+            // if the user does not enter a 5 character string it will automatically break the code
+
+            if (guess.length() != 5){
+
+                System.out.println("Sorry, you did not enter a word that has 5 characters! Your game will end now.");
+                break;
+
+            }
+
+            String hint = wordleGame.getHint(guess);
+            System.out.println("Here is your hint --> " + hint);
+
+            // if the user guesses the word it breaks the code and congratulates them
+
+            if (wordleGame.guessCorrect(guess) == true){
+
+                System.out.print("Good job! You guessed that the word was " + wordleGame.getSecretWord() + "!");
+                break;
+
+            } else if (i == 6) {
+
+                // if the user wasn't able to guess the word it gives them the word and then stops the guess loop
+
+                System.out.println("Sorry you are out of guesses.");
+                System.out.println("The word was " + wordleGame.getSecretWord());
+            }
+        }
+    }
+    public static String[] getWords() {
+        try {
+
+            // Opens the file "words1000.txt" through the pathname
+            File myObj = new File("words1000.txt");
+            Scanner myReader = new Scanner(myObj);
+
+            // Creates an array to store the words
+            String[] words = new String[1000];
+
+            // Puts the words from the file into an array
+            int counter = 0;
+            while (myReader.hasNextLine()) {
+                words[counter] = myReader.nextLine();
+                counter++;
+            }
+            myReader.close();
+
+            // Catches an error if there is one
+
+            return words;
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return new String[0];
+        }
+    }
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,2 +1,58 @@
 public class Wordle {
+
+    // generates the instance of Wordle and all variables: secretWord and Letters
+    private String secretWord;
+    private String letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+    public Wordle(String word){
+        this.secretWord = word;
+    }
+
+    // determines whether or not the user's guess was correct
+    public boolean guessCorrect(String guess){
+        return secretWord.equals(guess);
+    }
+    public String getHint(String guess){
+
+        // Generates the beginning of the hint and splits up the secret word and the guess so you can compare them
+
+        String hint = "";
+        String[] guessSplit = guess.split("");
+        String[] secretWordSplit = secretWord.split("");
+
+        /* First, compares the guess of the user and the secret word
+           Second, generates the hint based on the comparison
+           Lastly, removes the letters in the letter string that don't need to be there */
+
+        for (int i = 0; i < 5; i++){
+            if (guessSplit[i].equals(secretWordSplit[i])){
+
+                // if the letter exists and is in the right spot it adds  to the letter to the hint
+
+                hint += guessSplit[i];
+
+            } else if (secretWord.contains(guessSplit[i]) == true){
+
+                // if the letter exists but is not in the right spot it will add + to the hint
+
+                hint += "+";
+
+            } else {
+
+                // if the letter doesn't exist at all it will replace the excess letter in the letter list then add * to the hint
+
+                hint += "*";
+                letters = letters.replace(guessSplit[i],"");
+
+            }
+        }
+
+        // returns the hint that was generated based on the users guess
+
+        return hint;
+    }
+
+    // generates both the methods for getting the secret word and getting the letter list
+    public String getSecretWord() { return secretWord; }
+
+    public String getLetters() { return letters; }
 }
